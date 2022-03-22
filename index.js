f5b3bc46b357cb4fc071816065f007d5d4a348e6
raoakif <61361037+RaoAkif@users.noreply.github.com>
2022-03-23 01:20:47 +0500

    Update index.js

--- index.js
+++ index.js
@@ -60,6 +60,5 @@ document.querySelector('#book-list-table').addEventListener('click', (e) => {
 
   // Remove book from store
   Store.removeBook(
-    e.target.parentElement.parentElement.parentElement.firstElementChild.textContent
-  );
+    e.target.parentElement.parentElement.parentElement.firstElementChild.textContent);
 });
